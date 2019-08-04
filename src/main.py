1ef84a10e0ae7b699c8049b94762a53e17d63f29
Hernán García <hernanjkd@gmail.com>
2019-08-04 13:51:39 +0000

    f string

--- src/main.py
+++ src/main.py
@@ -103,4 +103,9 @@ if __name__ == '__main__':
     PORT = int(os.environ.get('PORT', 3000))
     app.run(host='0.0.0.0', port=PORT)
 
-print('Hello')
+
+a = 'Homer'
+b = 54
+
+
+print(f'{a} is {b}')
